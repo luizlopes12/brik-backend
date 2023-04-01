ddfc607f3c37220570acab7b7fb47c8c6c3e5cdb
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-04-01 01:39:58 -0300

    User endpoints

--- api/Controllers/authController.js
+++ api/Controllers/authController.js
@@ -44,7 +44,14 @@ class authController {
                 if(result){
                     let token = jwt.sign({id: verifyData.id}, process.env.JWT_SECRET, { expiresIn: '1h'})
                     let refreshToken = jwt.sign({id: verifyData.id}, process.env.JWT_SECRET, { expiresIn: '10d'})
-                    res.status(200).json({name: verifyData.name, email: verifyData.email, phone: verifyData.phone, token, refreshToken})
+                    res.status(200).json({name: verifyData.name, email: verifyData.email, phone: verifyData.phone, token, refreshToken,
+                    admin: verifyData.admin,
+                    editDivisions: verifyData.editDivisions,
+                    editLots: verifyData.editLots,
+                    editPartners: verifyData.editPartners,
+                    editBanners: verifyData.editBanners,
+                    editTaxes: verifyData.editTaxes,
+                  })
                   }else{
                     res.status(403).json({message: 'Email ou senha inválidos'})
                   }
@@ -56,6 +63,55 @@ class authController {
         }
 
     }
+
+    static listAllUsers = async (req, res) => {
+          
+        let users = await User.findAll({
+          attributes: [
+            'name',
+            'email',
+            'admin',
+            'editDivisions',
+            'editLots',
+            'editPartners',
+            'editBanners',
+            'editTaxes',
+          ],
+        });
+        
+        if(users.length === 0){
+            res.status(404).json({message: 'Nenhum usuário encontrado'})
+        }
+        res.status(200).json(users)
+    }
+    static updateUser = async (req, res) => {
+        const { id, name, email, phone, admin, editDivisions, editLots, editPartners, editBanners, editTaxes } = req.body
+        if(id && name && email && phone && admin !== undefined && editDivisions !== undefined && editLots !== undefined && editPartners !== undefined && editBanners !== undefined && editTaxes !== undefined){
+            let user = await User.update({
+                name: name,
+                email: email,
+                phone: phone,
+                admin: admin,
+                editDivisions: editDivisions,
+                editLots: editLots,
+                editPartners: editPartners,
+                editBanners: editBanners,
+                editTaxes: editTaxes,
+            }, {
+                where: {
+                    id: id
+                }
+            })
+            if(user[0] === 0){
+                res.status(404).json({message: 'Usuário não encontrado'})
+            }else{
+                res.status(200).json({message: 'Usuário atualizado com sucesso'})
+            }
+        }else{
+            res.status(206).json({message: 'Informações insuficientes'})
+        }
+    }
+
 }
 
 module.exports = authController
--- api/Routes/authRoutes.js
+++ api/Routes/authRoutes.js
@@ -3,7 +3,10 @@ const authController = require('../Controllers/authController.js')
 const router = express.Router()
 
 router
+.get('/users/list', authController.listAllUsers)
+.post('/users/update', authController.updateUser)
 .post('/login', authController.userLogin)
 .post('/register', authController.userRegistration)
+
 
 module.exports = router

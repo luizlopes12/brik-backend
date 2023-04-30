3977b2598e90d49e3c51d4e68719f0cb27f3ce69
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2023-04-30 10:58:29 -0300

    Update emailController.js

--- api/Controllers/emailController.js
+++ api/Controllers/emailController.js
@@ -15,21 +15,41 @@ class emailController {
       }
     });
 
-    let replyList = [process.env.EMAIL_REPLYER1, process.env.EMAIL_REPLYER2]
-
+    let replyList = [process.env.EMAIL_REPLYER1, process.env.EMAIL_REPLYER2];
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(!validRegex.test(email)){
+        res.status(400).json({message: 'Email inválido.'});
+    }
     const mailOptions = {
-      from: email,
-      to: process.env.EMAIL_USER,
-      replyTo: replyList,
-      subject: 'Solicitação de contato',
-      html: `<p>Olá, meu nome é ${name}, gostaria de entrar em contato para mais informações.</p>`
-    };
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        replyTo: replyList,
+        subject: 'Solicitação de contato',
+        html: `
+        <p>Olá, meu nome é ${name.split(' ')[0]}, gostaria de entrar em contato para mais informações.</p>
+        <p>Atenciosamente, <br> ${name} <br> ${email}</p>
+        `
+      };
+
+    const mailToUserOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        replyTo: replyList,
+        subject: 'Solicitação de contato',
+        html: `
+        <p>Olá ${name.split(' ')[0]}, obrigado por entrar em contato!</p>
+        <p>Caso queira ter algum detalhe sobre um lote, loteamento ou parceria <br> Sinta-se livre para entrar em contato.</p>
+        <p>Atenciosamente, <br> Atendimento BRIK <br> ${process.env.EMAIL_USER}</p>
+        `
+        };
+        
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log(error);
         res.status(500).json({message: 'Erro ao enviar email.'});
       } else {
+        transporter.sendMail(mailToUserOptions, (error, info) => {});
         res.status(200).json({message: 'Email enviado com sucesso!'});
       }
     });
